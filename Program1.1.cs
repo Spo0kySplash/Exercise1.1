04acb320f638761caef47fc26e9143fc5a166cda
Spo0kySplash <47415613+Spo0kySplash@users.noreply.github.com>
2019-02-08 08:53:31 +0200

    Add files via upload

---
+++ Program1.1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 238;
+            int b = 345;
+            int min = Math.Min(a, b);
+            if (min == a)
+            {
+                Console.WriteLine("b is the biggest number: " + b);
+            }
+            else
+            {
+                Console.WriteLine("a is biggest number: " + a);
+            }
+        }
+    }
+}
