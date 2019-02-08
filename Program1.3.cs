934dbd7415a24a53100eb8c49b2e7871d5b26323
Spo0kySplash <47415613+Spo0kySplash@users.noreply.github.com>
2019-02-08 08:55:05 +0200

    Add files via upload

---
+++ Program1.3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 238;
+            int b = 345;
+            if (a > b)
+            {
+                Console.WriteLine("The Biggest number is:" + a);
+            }
+            else
+            {
+                Console.WriteLine("The Biggest number is: " + b);
+            }
+        }
+    }
+}
